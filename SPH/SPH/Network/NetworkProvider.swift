6e52c76cb9d475c5c9e1ffcca884aa447195cb07
Derick <21DerickDerick@gmail.com>
2020-05-21 21:07:17 +0800

    Setup Network layer

---
+++ SPH/SPH/Network/NetworkProvider.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkProvider.swift
+//  SPH
+//
+//  Created by Derick on 21/5/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import Alamofire
+
+class NetworkProvider {
+    static var shared = NetworkProvider()
+    
+    typealias JSON = [String: Any]
+    
+    func request(url: String, type: HTTPMethod, headers: [String: String], body: [String: Any]?, completion: @escaping (JSON?, AFError?) -> Void) {
+        
+        if !ReachabilityManager.shared.hasConnection {
+            // Todo: Show Alert
+            return
+        }
+        
+        guard let url = URL(string: url) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = type.rawValue
+        request.timeoutInterval = 60
+        
+        let httpHeaders = HTTPHeaders(headers)
+        request.headers = httpHeaders
+        
+        if let body = body {
+            let jsonData = try? JSONSerialization.data(withJSONObject: body)
+            request.httpBody = jsonData
+        }
+        
+        AF.request(request)
+            .validate()
+            .responseJSON(completionHandler: { (response) in
+                
+                print("DEBUGD: \(response)")
+                
+                switch response.result {
+                case .failure(let error):
+                    completion(nil, error)
+                case .success(let value):
+                    let json = value as! JSON
+                    completion(json, nil)
+                }
+        })
+    }
+}
---
+++ SPH/SPH/Network/ReachabilityManager.swift
@@ -0,0 +1,31 @@
+//
+//  ReachabilityManager.swift
+//  SPH
+//
+//  Created by Derick on 21/5/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import Network
+
+class ReachabilityManager {
+    static var shared = ReachabilityManager()
+    
+    let monitor = NWPathMonitor()
+    var hasConnection = false
+    
+    func start() {
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                self.hasConnection = true
+                print(self.hasConnection)
+            } else {
+                self.hasConnection = false
+                print(self.hasConnection)
+            }
+        }
+        
+        let queue = DispatchQueue(label: "Monitor")
+        monitor.start(queue: queue)
+    }
+}
