2b4217779fddda5bf99127d59f6a153df9e860f9
Derick <21DerickDerick@gmail.com>
2020-05-21 19:49:30 +0800

    Create Project

---
+++ SPH/SPH/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SPH
+//
+//  Created by Derick on 21/5/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
