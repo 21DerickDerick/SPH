738264144add68aa26be16f05354f283dd17fd0b
Derick <21DerickDerick@gmail.com>
2020-05-22 22:15:37 +0800

    Add BaseTableViewCell unit test

---
+++ SPH/SPHTests/UIComponents/TableViews/BaseTableViewCellTests.swift
@@ -0,0 +1,29 @@
+//
+//  BaseTableViewCellTests.swift
+//  SPHTests
+//
+//  Created by Derick on 22/5/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import XCTest
+@testable import SPH
+
+class BaseTableViewCellTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSetupUI() {
+        let cell = BaseTableViewCell()
+        cell.awakeFromNib()
+        
+        XCTAssertTrue(cell.selectionStyle == .none)
+    }
+
+}
